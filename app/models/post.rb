e4fcdee887417713192ac6b2219559f81609b065
TT <tos.10041105@icloud.com>
2021-03-02 21:54:42 +0900

    add like function

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,14 @@ class Post < ApplicationRecord
 
   has_many :photos, dependent: :destroy
 
+  has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
+
   # 親から子を作成したり保存する
   accepts_nested_attributes_for :photos
+
+  def liked_by(user)
+    # user_idとpost_idが一致するlikeを検索する
+    Like.find_by(user_id: user.id, post_id: id)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i(new create index show destroy) do
     resources :photos, only: %i(create)
+
+    resources :likes, only: %i(create destroy)
   end
 
 end
