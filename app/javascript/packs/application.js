ae8386c8016a1b2aca5ddae930ef7cf608edf7cc
TT <tos.10041105@icloud.com>
2021-03-02 14:03:34 +0900

    add devise profile

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,9 @@
 import 'bootstrap';
 import '../stylesheets/application';
 
+import '@fortawesome/fontawesome-free/js/all';
+
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users,
+  controllers: { registrations: 'registrations' }
+
   root 'pages#home'
+
+  get '/users/:id', to: 'users#show', as: 'user'
 end
